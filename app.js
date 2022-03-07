d2bfc6e0a1ecb4bb8df2f0eed4c79a1df060aab1
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-07 11:00:48 -0500

    Make stateless component with hardcoded items

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// TODO
+// TODO: Inside app.js, create a GroceryList component that contains an
+// unordered list of 2 grocery items. Render this component to the div tag in
+// index.html with an id of app
+
+
+const GroceryList = function () {
+  return (
+    <ul>
+      <li>Frozen Pizza</li>
+      <li>Sea Salt Caramel Gelato</li>
+    </ul>
+  );
+}
+
+ReactDOM.render(GroceryList, document.getElementById('app'));
