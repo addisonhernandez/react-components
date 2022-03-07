3987d3c6cd2c3d2b838c0fd7f6260b8c0e603614
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-07 11:10:29 -0500

    Refactor using nested components

--- app.js
+++ app.js
@@ -2,14 +2,19 @@
 // unordered list of 2 grocery items. Render this component to the div tag in
 // index.html with an id of app
 
+const FrozenPizza = () => (
+  <li>Frozen Pizza</li>
+);
 
-const GroceryList = function () {
-  return (
-    <ul>
-      <li>Frozen Pizza</li>
-      <li>Sea Salt Caramel Gelato</li>
-    </ul>
-  );
-}
+const CaramelGelato = () => (
+  <li>Sea Salt Caramel Gelato</li>
+);
 
-ReactDOM.render(GroceryList, document.getElementById('app'));
+const GroceryList = () => (
+  <ul>
+    <FrozenPizza />
+    <CaramelGelato />
+  </ul>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
