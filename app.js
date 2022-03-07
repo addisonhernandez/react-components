3fcbb3193417b361e46fd96ceeec8587efc24edf
Addison Hernandez <addison.hernandez@gmail.com>
2022-03-07 11:21:55 -0500

    Add ability to dynamically render a grocery item

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-// TODO: Inside app.js, create a GroceryList component that contains an
-// unordered list of 2 grocery items. Render this component to the div tag in
-// index.html with an id of app
+// Inside app.js, create a GroceryList component that contains an
+// unordered list of 2 grocery items. Render this component to
+// the div tag in index.html with an id of app
+
+const GroceryListItem = (props) => (
+  <li>{ props.itemName }</li>
+);
 
 const FrozenPizza = () => (
-  <li>Frozen Pizza</li>
+  <GroceryListItem itemName='Frozen Pizza'/>
 );
 
 const CaramelGelato = () => (
-  <li>Sea Salt Caramel Gelato</li>
+  <GroceryListItem itemName='Sea Salt Caramel Gelato' />
 );
 
 const GroceryList = () => (
